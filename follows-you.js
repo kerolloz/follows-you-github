bb2993abf8fdb709d7a8ac11a04c1621f3298fee
eslam227863 <eslam227863@f-eng.tanta.edu.eg>
2020-05-10 17:38:13 +0300

    simple-Ifollow-label

--- follows-you.js
+++ follows-you.js
@@ -10,11 +10,13 @@ async function isFollowing(x, y) {
     `https://api.github.com/users/${x}/following/${y}`,
     { method: "GET" }
   );
-  return response.status === 204; // according to github docs 204 means follows
+  return response.status === 204 ? 204 :false; // according to github docs 204 means follows
 }
 
 class FollowsYou {
-  constructor() {
+  constructor(text,color) {
+    this.text=text ;
+    this.color=color;
     this.element = this.createElement();
     this.stickyElement = this.createElement("m-1"); // give it some margin
   }
@@ -26,13 +28,14 @@ class FollowsYou {
    */
   createElement(...classes) {
     const el = document.createElement("span");
-    el.innerText = "follows you";
+    el.innerText = this.text;
     el.style.fontSize = "10px";
+    el.style.backgroundColor=this.color;
     el.classList.add(
       ...classes,
       "label",
       "text-uppercase",
-      "bg-gray-dark",
+     // "bg-gray-dark",
       "v-align-middle"
     );
     return el;
@@ -53,6 +56,7 @@ class FollowsYou {
 }
 
 let shouldShowFollowsYou = false;
+let shouldShowIFollow = false;
 
 (async () => {
   const isProfilePage = !!$(".vcard-fullname");
@@ -60,17 +64,28 @@ let shouldShowFollowsYou = false;
   const openedProfileUsername = $("meta[property='profile:username']").content;
 
   // if I'm logged in and viewing some user's profile page who follows me
-  shouldShowFollowsYou =
-    isProfilePage &&
-    loggedInUsername !== openedProfileUsername && // not my profile
-    (await isFollowing(openedProfileUsername, loggedInUsername));
+  shouldShowFollowsYou = isProfilePage &&loggedInUsername !== openedProfileUsername && // not my profile
+  (await isFollowing(openedProfileUsername, loggedInUsername));
+
+  shouldShowIFollow = isProfilePage &&loggedInUsername !== openedProfileUsername && // not my profile
+  (await isFollowing( loggedInUsername,openedProfileUsername));
 
   if (shouldShowFollowsYou) {
-    const f = new FollowsYou();
-    const showFollowsYou = f.showElements.bind(f);
+    const followMe = new FollowsYou('Follow-Me','#656565');
+    const showFollowsYou = followMe.showElements.bind(followMe);
     const PROFILE_TAB_SWITCH = "pjax:end";
-
     showFollowsYou();
     document.addEventListener(PROFILE_TAB_SWITCH, showFollowsYou);
+ 
+  } 
+   if(shouldShowIFollow){
+    const ifollow = new FollowsYou('I-Follow','#d01919');
+    const showIFollow  = ifollow.showElements.bind(ifollow);
+    const PROFILE_TAB_SWITCH = "pjax:end";
+    showIFollow();
+    document.addEventListener(PROFILE_TAB_SWITCH, showIFollow);
   }
+
+
+
 })();
